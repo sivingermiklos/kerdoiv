84461da8c704ac53e03683a6f09b8012392405fe
sivingermiklos <83176923+sivingermiklos@users.noreply.github.com>
2022-12-13 11:56:54 +0100

    javítás

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ function ellenoriz() {
     && document.getElementById("kerdes4_2").checked
     && document.getElementById("kerdes5_1").checked
     && document.getElementById("kerdes6_1").checked) {
+        document.getElementById("lose").style.display = "none";
+        document.getElementById("win").style.display = "block";
         document.getElementById("win").innerHTML = sentence_win + '<img src="'+img.win+'" />';  
     }
     else {
+        document.getElementById("win").style.display = "none";
+        document.getElementById("lose").style.display = "block";
         document.getElementById("lose").innerHTML = sentence_lose + '<img src="'+img.lose+'" />';
     }
 }
